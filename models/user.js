2511c0a9514605c11aa4a24e4e46d40c3584fda4
Faddah Wolf <my_biz@me.com>
2017-03-05 23:40:58 +0000

    added bcryptjs to npm node_modules and code in user.js to hash/encrypt the password. pushing to github repository.

--- models/user.js
+++ models/user.js
@@ -28,10 +28,8 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback) {
-    var bcrypt = require('bcryptjs');
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(newUser.password, salt, function(err, hash) {
-            // Store hash in your password DB
             newUser.password = hash;
             newUser.save(callback);
         });
